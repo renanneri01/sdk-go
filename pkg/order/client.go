5bd700ca3364c36e9635e9945202bb7c2119da1c
Renan Neri <renan.neri@mercadolivre.com>
2025-01-13 18:36:25 -0300

    Added documentation

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -12,14 +12,20 @@ const (
 	urlBase = "https://api.mercadopago.com/v1/orders"
 )
 
+// Client contains the methods to interact with the Order API.
 type Client interface {
+	// Create creates a new order.
+	// It is a post request to the endpoint: https://api.mercadopago.com/v1/orders
+	// Reference: https://www.mercadopago.com.br/developers/pt/reference/order/online-payments/create/post
 	Create(ctx context.Context, request Request) (*Response, error)
 }
 
+// client is the implementation of Client.
 type client struct {
 	cfg *config.Config
 }
 
+// NewClient returns a new Order API Client.
 func NewClient(c *config.Config) Client {
 	return &client{
 		cfg: c,
